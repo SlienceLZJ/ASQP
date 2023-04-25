840d1c9c654cc53cb9b01e91e0ca24cd3a05541b
Slience <53400816+SlienceLZJ@users.noreply.github.com>
2023-04-25 14:35:37 +0800

    Delete download.py

--- download.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-tokenizer = AutoTokenizer.from_pretrained("t5-base")
-model = AutoModelForSeq2SeqLM.from_pretrained("t5-base")
-
-tokenizer.save_pretrained("pretrained_models/t5-base")
-model.save_pretrained("pretrained_models/t5-base")
